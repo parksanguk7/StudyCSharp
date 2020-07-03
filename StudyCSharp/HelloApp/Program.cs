6b5660d4260f4440597e6c5c2567665f85d86fc7
Parksanguk7 <swsw1717@naver.com>
2020-07-03 14:27:00 +0900

    HelloApp 완료

--- StudyCSharp/HelloApp/Program.cs
+++ StudyCSharp/HelloApp/Program.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using static System.Console;
 
 namespace HelloApp
 {
@@ -10,6 +6,13 @@ namespace HelloApp
     {
         static void Main(string[] args)
         {
+            if (args.Length == 0)
+            {
+                WriteLine("ex: HelloApp.exe<이름>");
+                return;
+            }
+           
+            WriteLine($"Hello,{args[0]}!");
         }
     }
 }
